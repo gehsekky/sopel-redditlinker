dc9fdc9c8fd6c12f839f0ebd952f4eea40421779
Andy Chung <gehsekky@gmail.com>
2014-09-30 00:07:52 -0400

    Added some checks for required settings in config file.

--- redditlinker.py
+++ redditlinker.py
@@ -28,14 +28,35 @@ import urllib2
 @willie.module.commands('redditlinker')
 @rate(5)
 def redditlinker(bot, trigger):
+	# check to see if redditlinker section is in config
+	if not hasattr(bot.config, 'redditlinker'):
+		raise ValueError('No config section for redditlinker detected. Please make sure you set the options in the willie config file.')
+
+	# check for message setting. if none, provide default
+	if not bot.config.redditlinker.message:
+		bot.config.redditlinker.message = 'a script to post links from irc to a subreddit - https://github.com/gehsekky/willie-redditlinker'
 	bot.say(bot.config.redditlinker.message)
 
 @rule(r'.*?((http[s]?|ftp)://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)')
 def redditlinkerlistener(bot, trigger):
-	try:
-		# extract link from text
-		url = trigger.group(1)
+	# check to see if redditlinker section is in config
+	if not hasattr(bot.config, 'redditlinker'):
+		raise ValueError('No config section for redditlinker detected. Please make sure you set the options in the willie config file.')
 
+	# checks for required settings
+	if not bot.config.redditlinker.username:
+		raise ValueError('No config setting for username found. Please enter the username of the reddit account to post from (karma should be high enough so it doesn\'t need to do captcha.')
+
+	if not bot.config.redditlinker.password:
+		raise ValueError('No config setting for password found. Please enter the password of the reddit account to post from.')
+
+	if not bot.config.redditlinker.subreddit:
+		raise ValueError('No config setting for subreddit found. Please enter the subreddit to post to.')
+
+	# extract link from text
+	url = trigger.group(1)
+
+	try:
 		# go to link and see if there is title
 		response = urllib2.urlopen(url)
 		if response.headers.maintype != 'image':
@@ -52,4 +73,4 @@ def redditlinkerlistener(bot, trigger):
 			subreddit = reddit.get_subreddit(bot.config.redditlinker.subreddit)
 			subreddit.submit(title, url=url)
 	except:
-		pass # stealth mode. makes it hard to debug though so remove it.
+		pass
